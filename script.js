7484c079de9fbc2bb59b8bef8ac4fd64fe985849
iiMorph <gameknight999@hotmail.com>
2018-09-20 20:37:07 +0800

    another commit of the day.

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
     Filename: script.js
     Open Source on Github, available at github.com/Morphoxeris.
 */
+
+const thisText = "Hello, World!"
+const textArray = thisText.split(' ').slice(1);
+console.log(textArray);
+console.log(thisText);
